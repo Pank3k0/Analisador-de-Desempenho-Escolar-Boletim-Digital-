906be56f59bd9b552564bee7e1e17b03be36f9fe
Pank3k0 <jnkjho3@gmail.com>
2026-05-04 12:16:50 -0300

    melhoramos :3

---
+++ main.py
@@ -0,0 +1,16 @@
+aluno_nome = input('Digite seu nome: ').lower()
+
+
+
+while True:
+    print('Analisador-de-Desempenho-Escolar-Boletim-Digital')
+
+    print('1. Alunos')
+    
+    print('2. Avaliações')
+
+    print('3. Boletim')
+
+    print('Sair')
+
+    opcao = input(f'Olá, {aluno_nome} escolha a opção desejada: ')
