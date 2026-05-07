cb72bf7e2f16eef9cb846f38c4ebe336356ac79d
Pank3k0 <jnkjho3@gmail.com>
2026-05-07 11:51:44 -0300

    :P

--- main.py
+++ main.py
@@ -1,29 +1,60 @@
-aluno_nome = input('Digite seu nome: ').lower()
+aluno_nome = input('Digite seu nome: ').upper()
 
-
+def text():
+    print("__________       .__          __  .__          ________  .__       .__  __         .__   ")
+    print("\\______   \\ ____ |  |   _____/  |_|__| _____   \\______ \\ |__| ____ |__|/  |______  |  |  ")
+    print(" |    |  _//  _ \\|  | _/ __ \\   __\\  |/     \\   |    |  \\|  |/ ___\\|  \\   __\\__  \\ |  |  ")
+    print(" |    |   (  <_> )  |_\\  ___/|  | |  |  Y Y  \\  |    `   \\  / /_/  >  ||  |  / __ \\|  |__")
+    print(" |______  /\\____/|____/\\___  >__| |__|__|_|  / /_______  /__\\___  /|__||__| (____  /____/")
+    print("        \\/                 \\/              \\/          \\/  /_____/               \\/      ")
 
 while True:
-    print('Analisador-de-Desempenho-Escolar-Boletim-Digital')
-
+    text()
+    print('===============================================')
+    print('')
     print('1. Alunos')
-    
+    print('')
     print('2. Avaliações')
-
+    print('')
     print('3. Boletim')
-
-    print('Sair')
-
+    print('')
+    print('0. Sair')
+    print('')
+    print('===============================================')
     opcao = int(input(f'Olá, {aluno_nome} escolha a opção desejada: '))
 
 # aluno, se possivel nao mexam, nao sei como esta funcionando
 
     if opcao == 1:
         print(f'ALuno: {aluno_nome}')
-        print('digite 0 para voltar')
+        print('Digite 0 para voltar')
 
         opcao_diferente1 = int(input(f'{aluno_nome}, você deseja retornar?  '))
         if opcao_diferente1 == 0:
             continue
 
-# acabou o aluno e começa a avaliação
+# acabou o aluno e começa a saida pq eu quero
 
+    if opcao == 0:
+        break
+
+# agr a avaliação >:3
+
+    if opcao == 2:
+        print('Sentimos muito, essa opção esta indisponivel no momento')
+        print('Pressione 0 para voltar')
+
+        opcao_diferente1 = int(input(f'{aluno_nome}, você deseja retornar?  '))
+        if opcao_diferente1 == 0:
+            continue
+
+# Boletim TwT
+
+    if opcao == 3:
+        print('Sentimos muito, essa opção esta indisponivel no momento ')
+        print('Pressione 0 para voltar')
+    
+
+    opcao_diferente1 = int(input(f'{aluno_nome}, você deseja retornar? '))
+    if opcao_diferente1 == 0:
+        continue
