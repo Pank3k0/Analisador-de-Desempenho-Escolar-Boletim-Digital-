dd3675cb8b8fb651064cd514aec812c9fa8673ae
Pank3k0 <jnkjho3@gmail.com>
2026-05-07 09:30:32 -0300

    :3

--- main.py
+++ main.py
@@ -13,4 +13,17 @@ while True:
 
     print('Sair')
 
-    opcao = input(f'Olá, {aluno_nome} escolha a opção desejada: ')
+    opcao = int(input(f'Olá, {aluno_nome} escolha a opção desejada: '))
+
+# aluno, se possivel nao mexam, nao sei como esta funcionando
+
+    if opcao == 1:
+        print(f'ALuno: {aluno_nome}')
+        print('digite 0 para voltar')
+
+        opcao_diferente1 = int(input(f'{aluno_nome}, você deseja retornar?  '))
+        if opcao_diferente1 == 0:
+            continue
+
+# acabou o aluno e começa a avaliação
+
