78343d909b344b337d5624147468c6832449f6b3
m1tzy-tbh <m1tzy7194@gmail.com>
2026-06-25 11:04:47 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- main.py
+++ main.py
@@ -1,29 +1,68 @@
 import os
 
-def error():
-        print('===============================================================')
-        print('')
-        print('   Sentimos muito, essa opção esta indisponivel no momento')
-        print('')
-        print('===============================================================')
-        print('Pressione 0 para voltar')
+alunos = [['amanda soninho']]
+notas = []
+boletim = []
+
+def lista(tipo):
+    if tipo == 'aluno':
+        for aluno in alunos:
+            print(f'Aluno: {aluno}[0], - {aluno[1]}')
+    elif tipo == 'nota':
+        for nota in notas:
+            print(f'Nota : {nota[0]} - {nota[1]} - {nota[2]}')
+
+# função pra adicionar coisas nas listas
+
+def add(tipo):
+    if tipo == 'alunos':
+        codigo = len(alunos) + 1
+        aluno = input('Digite o nome do aluno: ')
+        alunos.append([aluno])
+    elif tipo == 'notas':
+        codigo = len(notas) + 1
+        nota1 = float(input('digite a nota do 1° bimestre: '))
+        nota2 = float(input('digite a nota do 2° bimestre: '))
+        notas.append([nota1, nota2])
+    elif tipo == 'boletim':
+        lista('aluno')
+        sniggers = int(input('digite o codigo do aluno: '))
+        codigo = len(boletim) + 1
+
+# função dos menus
 
 def menus(menu):
     if menu == 'principal':
-        print('===============================================')
+        print('+-------------------------+\n')
+        print('1. Alunos\n')
+        print('2. Avaliações\n')
+        print('3. Boletim\n')
+        print('0. Sair\n')
+        print('+-------------------------+\n')
+    elif menu == 'avaliacao':
+        print('+-----------------------+\n')
+        print('1.Adicionar notas\n')
+        print('2.Ver notas cadastradas\n')
+        print('0.Voltar\n')
+        print('+-----------------------+\n')
         print('')
-        print('1. Alunos')
-        print('')
-        print('2. Avaliações')
-        print('')
-        print('3. Boletim')
-        print('')
-        print('0. Sair')
-        print('')
-        print('===============================================')
-        print('')
-
-aluno_nome = input('Digite seu nome: ').upper()
+    elif menu == 'alunos':
+        print('+-----------------------------+\n')
+        print('1.Adicionar alunos\n')
+        print('2.Ver alunos\n')
+        print('0.Voltar\n')
+        print('+-----------------------------+\n')
+    elif menu == 'adnotas':
+        print('+----------------------+\n')
+        print('Adicionar notas\n')
+        print('+----------------------+\n')
+    elif menu == 'Boletim':
+        print('\nBoletem Digital\n')
+        print('+---------------------+\n')
+        print('1.Criar Boletim\n')
+        print('2.Ver Boletins\n')
+        print('0.Voltar\n')
+        print('+---------------------+\n')
 
 # função para limpar a tela
 
@@ -45,29 +84,34 @@ def show_text():
     
 
 while True:
-    print('')
-    print('')
+    clear_screen()
     show_text()
     menus('principal')
-    opcao = int(input(f'Olá, {aluno_nome} escolha a opção desejada: '))
+    opcao = int(input('Olá, escolha a opção desejada: '))
 
-# aluno, se possivel nao mexam, nao sei como esta funcionando
-
+#  aluno. Se possivel nao mexam, nao sei como esta funcionando
     if opcao == 1:
         clear_screen()
-        print('===============================================================')
-        print('')
-        print(f'ALuno: {aluno_nome}')
-        print('')
-        print('===============================================================')
-        print('Digite 0 para voltar')
-        opcao_diferente1 = int(input(f'{aluno_nome}, você deseja retornar?  '))
-        if opcao_diferente1 == 0:
-            clear_screen()
-        continue
+        menus('alunos')
+        escolha = int(input('Selecione a opção desejada: '))
 
-    else:
-            print('opção invalida, por favor, tente novamente')
+        if  escolha == 1:
+            add('alunos')
+            clear_screen()
+
+        elif escolha == 2:
+            print('\nAlunos Cadastrados\n')
+
+            if len(alunos) == 0:
+                print('Nenhum aluno cadastrado')
+
+            else:
+                clear_screen()
+                lista('aluno')
+                input('\nP' \
+                'ressione ENTER para voltar')
+                clear_screen()
+                continue
 
 # acabou o aluno e começa a saida pq eu quero
 
@@ -78,24 +122,42 @@ while True:
 
     if opcao == 2:
         clear_screen()
-        error()
-        opcao_diferente1 = int(input(f'{aluno_nome}, você deseja retornar?  '))
-        clear_screen()
-        if opcao_diferente1 == 0:
-            continue
+        menus('avaliacao')
+        escolha2 = int(input('selecione a opção que desejar: '))
 
-    else:
-            print('opção invalida, por favor, tente novamente')
+        if escolha2 == 1:
+            clear_screen()
+            menus('adnotas')
+            add('notas')
+            clear_screen()
+        
+        elif escolha2 == 2:
+            clear_screen()
+            print('Notas Cadastradas\n')
+            
+            if len(notas) == 0:
+                clear_screen()
+                print('Nenhuma nota cadastrada')
+                input('pressione ENTER para voltar ')
+            else:
+                lista('nota')
+                input('\npressione ENTER para voltar ')
+
+
 
 # Boletim TwT
 
     if opcao == 3:
         clear_screen()
-        error()
-    opcao_diferente1 = int(input(f'{aluno_nome}, você deseja retornar? '))
-    clear_screen()
-    if opcao_diferente1 == 0:
-        continue
+        menus('Boletim')
+        opcao3 = int(input('\nSelecione a opção desejada: '))
+        
+        if opcao3 == 1:
+            clear_screen()
+            for aluno_boletim in alunos:
+                print(f'aluno{}')
 
-    else:
-            print('opção invalida, por favor, tente novamente')
+            clear_screen()
+            print('Boletim criado com sucesso?')
+            input('\nPressione ENTER para voltar')
+            clear_screen()
